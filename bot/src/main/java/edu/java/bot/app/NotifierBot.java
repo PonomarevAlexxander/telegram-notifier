bbcc5aec6a51a7bab2fae28ff96d91d3b0a782e8
Alexander Ponomarev <aaponomarev04@gmail.com>
2024-03-24 23:36:54 +0300

    Remove unneeded check

--- bot/src/main/java/edu/java/bot/app/NotifierBot.java
+++ bot/src/main/java/edu/java/bot/app/NotifierBot.java
@@ -10,7 +10,6 @@ import com.pengrad.telegrambot.request.DeleteMyCommands;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.request.SetMyCommands;
 import com.pengrad.telegrambot.response.BaseResponse;
-import edu.java.bot.client.dto.ApiErrorResponse;
 import edu.java.bot.command.Command;
 import edu.java.bot.service.CommandService;
 import jakarta.annotation.PostConstruct;
@@ -18,7 +17,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestClientResponseException;
 
 @Component
 public class NotifierBot implements Bot {
@@ -59,15 +57,7 @@ public class NotifierBot implements Bot {
     public int process(List<Update> updates) {
         for (var update : updates) {
             SendMessage request;
-            try {
-                request = service.process(update);
-            } catch (RestClientResponseException e) {
-                ApiErrorResponse errorResponse = e.getResponseBodyAs(ApiErrorResponse.class);
-                request = new SendMessage(
-                    update.message().chat().id(),
-                    String.format("Operation failed with error: %s", errorResponse.exceptionMessage())
-                );
-            }
+            request = service.process(update);
             execute(request);
         }
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
